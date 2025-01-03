cb1740c544e00682892bbff9081fc15472bdfdde
Arka Bhuiyan <belmont66@duck.com>
2025-01-03 11:31:19 +0600

    injected the service inside the producer main class, made the main class implement command line runner to make the service send a message to the queue.

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/RabbitmqProducerApplication.java
@@ -1,13 +1,26 @@
 package com.ablonewolf.rabbitmq_producer;
 
+import com.ablonewolf.rabbitmq_producer.service.RabbitMQProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RabbitmqProducerApplication {
+import java.util.concurrent.ThreadLocalRandom;
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqProducerApplication.class, args);
-	}
+@SpringBootApplication
+@RequiredArgsConstructor
+public class RabbitmqProducerApplication implements CommandLineRunner {
+
+    private final RabbitMQProducerService rabbitMQProducerService;
+
+    @Override
+    public void run(String... args) {
+        rabbitMQProducerService.sendMessage("Thread Name: " + ThreadLocalRandom.current().nextInt());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducerApplication.class, args);
+    }
 
 }
