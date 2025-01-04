ced7b2f9a89148a1700d32779237ad9c233f72ec
Arka Bhuiyan <belmont66@duck.com>
2025-01-04 21:15:38 +0600

    added a method to send incremented value of i at a fixed rate in a scheduled fashion; additonally, included a logger to log the incremented values of i.

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/service/RabbitMQProducerService.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/service/RabbitMQProducerService.java
@@ -1,15 +1,27 @@
 package com.ablonewolf.rabbitmq_producer.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RabbitMQProducerService {
     private final RabbitTemplate rabbitTemplate;
+    private static final Logger log = LoggerFactory.getLogger(RabbitMQProducerService.class);
+    private Integer i = 1;
 
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend("helloQueue", message);
+    }
+
+    @Scheduled(fixedRate = 500)
+    public void sendMessageAtFixedRate() {
+        log.info("i is {}", i);
+        rabbitTemplate.convertAndSend("fixedRate", "Current value of i is: " + i);
+        ++i;
     }
 }
