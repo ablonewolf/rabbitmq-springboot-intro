6d9fb60c753c48b271cce6dd030b8bc8df467fe4
Arka Bhuiyan <belmont66@duck.com>
2025-01-05 00:56:22 +0600

    added the method to publish employee object to the employee queue as a JSON object

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/producer/RabbitMQProducerComponent.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/producer/RabbitMQProducerComponent.java
@@ -1,5 +1,8 @@
 package com.ablonewolf.rabbitmq_producer.producer;
 
+import com.ablonewolf.rabbitmq_producer.model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +13,23 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RabbitMQProducerComponent {
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
     private static final Logger log = LoggerFactory.getLogger(RabbitMQProducerComponent.class);
 
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend("helloQueue", message);
     }
+
+    public void publishEmployeeToQueue(Employee employee) {
+        try {
+            var jsonObject = objectMapper.writeValueAsString(employee);
+            rabbitTemplate.convertAndSend("employeeQueue", jsonObject);
+        } catch (JsonProcessingException e) {
+            log.error("An error occurred while serializing employee to json, details: {}", e.getMessage());
+        }
+    }
+
+
 
 /*
     @Scheduled(fixedRate = 500)
