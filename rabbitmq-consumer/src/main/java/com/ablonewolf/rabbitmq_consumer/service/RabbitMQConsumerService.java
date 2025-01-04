4bb78543491b09b23a0ad4884a883d372085c707
Arka Bhuiyan <belmont66@duck.com>
2025-01-04 21:39:29 +0600

    added the consumer method in the service layer of the consumer application to concurrently consume the messages from the "fixedRate" queue and simulated a blocking scenario by making the thread sleep for 2 seconds so that the consumption operation takes time to complete.

--- rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/service/RabbitMQConsumerService.java
+++ rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/service/RabbitMQConsumerService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
 public class RabbitMQConsumerService {
 
@@ -13,5 +15,15 @@ public class RabbitMQConsumerService {
     @RabbitListener(queues = "helloQueue")
     public void listen(String message) {
         log.info("Consuming message: {}", message);
+    }
+
+    @RabbitListener(queues = "fixedRate", concurrency = "3")
+    public void listenFixedRate(String message) {
+        log.info("Thread {} consuming message: {}", Thread.currentThread().getName(), message);
+        try {
+            Thread.sleep(Duration.ofSeconds(2L));
+        } catch (InterruptedException e) {
+            log.error("An exception occurred while attempting to sleep, details: {}", e.getMessage());
+        }
     }
 }
