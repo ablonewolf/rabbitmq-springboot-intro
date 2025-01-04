c94e6e62c6dd7c819addd75af9cf41bf26f4b08b
Arka Bhuiyan <belmont66@duck.com>
2025-01-05 02:46:32 +0600

    injected the Bean of ObjectMapper to the RabbitMQConsumerComponent as well as added the listener method to consume Employee JSON from the employeeQueue.

--- rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/consumer/RabbitMQConsumerComponent.java
+++ rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/consumer/RabbitMQConsumerComponent.java
@@ -1,5 +1,9 @@
 package com.ablonewolf.rabbitmq_consumer.consumer;
 
+import com.ablonewolf.rabbitmq_consumer.model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -8,9 +12,11 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 
 @Component
+@RequiredArgsConstructor
 public class RabbitMQConsumerComponent {
 
     private static final Logger log = LoggerFactory.getLogger(RabbitMQConsumerComponent.class);
+    private final ObjectMapper objectMapper;
 
     @RabbitListener(queues = "helloQueue")
     public void listen(String message) {
@@ -24,6 +30,16 @@ public class RabbitMQConsumerComponent {
             Thread.sleep(Duration.ofSeconds(2L));
         } catch (InterruptedException e) {
             log.error("An exception occurred while attempting to sleep, details: {}", e.getMessage());
+        }
+    }
+
+    @RabbitListener(queues = "employeeQueue")
+    public void consumeEmployee(String message) {
+        try {
+            var employee = objectMapper.readValue(message, Employee.class);
+            log.info("Received employee object: {}", employee);
+        } catch (JsonProcessingException e) {
+            log.error("An exception occurred while deserializing employee object: {}", e.getMessage());
         }
     }
 }
