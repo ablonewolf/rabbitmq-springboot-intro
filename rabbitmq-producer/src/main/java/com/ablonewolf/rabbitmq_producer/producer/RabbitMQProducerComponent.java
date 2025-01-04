16ca85d5d5a33dc6780a1ff6f6582e71804e12f7
Arka Bhuiyan <belmont66@duck.com>
2025-01-04 23:57:52 +0600

    disabled the scheduled sending of the value of i at a fixed rate as well as commented out the method.

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/producer/RabbitMQProducerComponent.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/producer/RabbitMQProducerComponent.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,16 +11,17 @@ import org.springframework.stereotype.Component;
 public class RabbitMQProducerComponent {
     private final RabbitTemplate rabbitTemplate;
     private static final Logger log = LoggerFactory.getLogger(RabbitMQProducerComponent.class);
-    private Integer i = 1;
 
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend("helloQueue", message);
     }
 
+/*
     @Scheduled(fixedRate = 500)
     public void sendMessageAtFixedRate() {
         log.info("i is {}", i);
         rabbitTemplate.convertAndSend("fixedRate", "Current value of i is: " + i);
         ++i;
     }
+*/
 }
