a793d7cfc1262e3898b96f28490b634c5889be13
Arka Bhuiyan <belmont66@duck.com>
2025-01-04 23:51:00 +0600

    added a model class along with the associated package to demonstrate how we can publish message as JSON objects.

---
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/model/Employee.java
@@ -0,0 +1,22 @@
+package com.ablonewolf.rabbitmq_producer.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String employeeId;
+    private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+}
