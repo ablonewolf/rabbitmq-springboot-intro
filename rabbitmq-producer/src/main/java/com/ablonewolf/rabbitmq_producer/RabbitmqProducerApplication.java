97a94d1307b0dbc2250ee08997b3fbb58ede3748
Arka Bhuiyan <belmont66@duck.com>
2025-01-05 00:57:09 +0600

    generated 10 employee objects in the main class of the producer app and published them in the queue through the appropriate method.

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/RabbitmqProducerApplication.java
@@ -1,12 +1,15 @@
 package com.ablonewolf.rabbitmq_producer;
 
+import com.ablonewolf.rabbitmq_producer.model.Employee;
 import com.ablonewolf.rabbitmq_producer.producer.RabbitMQProducerComponent;
+import com.ablonewolf.rabbitmq_producer.util.RandomNameGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDate;
 import java.util.concurrent.ThreadLocalRandom;
 
 @EnableScheduling
@@ -19,6 +22,13 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         rabbitMQProducerComponent.sendMessage("Thread Name: " + ThreadLocalRandom.current().nextInt());
+
+        for (int i = 1; i <= 10; i++) {
+            var employee = new Employee(RandomNameGenerator.generateRandomId(),
+                RandomNameGenerator.generateRandomName(),
+                LocalDate.now());
+            rabbitMQProducerComponent.publishEmployeeToQueue(employee);
+        }
     }
 
     public static void main(String[] args) {
