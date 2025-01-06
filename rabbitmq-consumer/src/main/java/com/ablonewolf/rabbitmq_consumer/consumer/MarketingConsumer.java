abc61f261d84dda490136346b1b75502fafc34e9
Arka Bhuiyan <belmont66@duck.com>
2025-01-07 02:41:04 +0600

    removed the old RabbitMQConsumerComponent and added two new consumer components for both Accounting and Marketing respectively.

---
+++ rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/consumer/MarketingConsumer.java
@@ -0,0 +1,28 @@
+package com.ablonewolf.rabbitmq_consumer.consumer;
+
+import com.ablonewolf.rabbitmq_consumer.configuration.RabbitMQConfig;
+import com.ablonewolf.rabbitmq_consumer.model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MarketingConsumer {
+    private static final Logger log = LoggerFactory.getLogger(MarketingConsumer.class);
+    private final ObjectMapper objectMapper;
+
+    @RabbitListener(queues = RabbitMQConfig.MARKETING_QUEUE)
+    public void consumeEmployee(String message) {
+        try {
+            var employee = objectMapper.readValue(message, Employee.class);
+            log.info("Received employee object from Marketing Queue: {}", employee);
+        } catch (JsonProcessingException e) {
+            log.error("An exception occurred while deserializing employee object: {}", e.getMessage());
+        }
+    }
+}
