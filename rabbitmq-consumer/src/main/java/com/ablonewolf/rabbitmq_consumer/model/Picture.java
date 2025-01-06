e01f2f0361e5d80a296da79c9aa9fd1f17234ceb
Arka Bhuiyan <belmont66@duck.com>
2025-01-07 05:47:43 +0600

    removed unused and redundant annotations from the Picture class in the consumer app.

--- rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/model/Picture.java
+++ rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/model/Picture.java
@@ -1,16 +1,10 @@
 package com.ablonewolf.rabbitmq_consumer.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
-@AllArgsConstructor
-@Builder
 public class Picture {
     private String name;
     private String type;
