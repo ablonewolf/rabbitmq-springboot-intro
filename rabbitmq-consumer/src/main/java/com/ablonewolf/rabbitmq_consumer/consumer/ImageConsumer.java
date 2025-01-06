f5a7528776ae33b463f69b936cf45cf155e27710
Arka Bhuiyan <belmont66@duck.com>
2025-01-07 05:50:19 +0600

    Added consumer classes as well as concerned methods to consume picture objects from the image queue and the vector queue.

---
+++ rabbitmq-consumer/src/main/java/com/ablonewolf/rabbitmq_consumer/consumer/ImageConsumer.java
@@ -0,0 +1,28 @@
+package com.ablonewolf.rabbitmq_consumer.consumer;
+
+import com.ablonewolf.rabbitmq_consumer.configuration.RabbitMQConfig;
+import com.ablonewolf.rabbitmq_consumer.model.Picture;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ImageConsumer {
+    private static final Logger log = LoggerFactory.getLogger(ImageConsumer.class);
+    private final ObjectMapper objectMapper;
+
+    @RabbitListener(queues = RabbitMQConfig.PICTURE_IMAGES_QUEUE)
+    public void listen(String message) {
+        try {
+            var picture = objectMapper.readValue(message, Picture.class);
+            log.info("Image received: {}", picture);
+        } catch (JsonProcessingException e) {
+            log.error("An error occurred when parsing picture, details: {}", e.getMessage());
+        }
+    }
+}
