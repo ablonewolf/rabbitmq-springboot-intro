5dcbb26d511596a0296236b204d4961d2e3dc999
Arka Bhuiyan <belmont66@duck.com>
2025-01-07 04:20:34 +0600

    added code block to set up picture image and picture vector queue along with the picture exchange; additionally, bound the queues with the exchange with appropriate routing key.

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/configuration/RabbitMQConfig.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/configuration/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.ablonewolf.rabbitmq_producer.configuration;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
@@ -33,6 +34,13 @@ public class RabbitMQConfig {
     public static final String HR_EXCHANGE = "hr.exchange";
     public static final String ACCOUNTING_QUEUE = "accounting.queue";
     public static final String MARKETING_QUEUE = "marketing.queue";
+
+    public static final String PICTURE_EXCHANGE = "picture.exchange";
+    public static final String PICTURE_IMAGES_QUEUE = "queue.picture.image";
+    public static final String PICTURE_VECTOR_QUEUE = "queue.picture.vector";
+    public static final String JPG_ROUTING_KEY = "jpg";
+    public static final String PNG_ROUTING_KEY = "png";
+    public static final String SVG_ROUTING_KEY = "svg";
 
     @Bean
     public ConnectionFactory connectionFactory() {
@@ -79,6 +87,42 @@ public class RabbitMQConfig {
     @Bean
     public Binding marketingBinding() {
         return BindingBuilder.bind(marketingQueue()).to(getHRExchange());
+    }
+
+    @Bean
+    public Queue pictureImagesQueue() {
+        return new Queue(PICTURE_IMAGES_QUEUE, true);
+    }
+
+    @Bean
+    public Queue pictureVectorQueue() {
+        return new Queue(PICTURE_VECTOR_QUEUE, true);
+    }
+
+    @Bean
+    public DirectExchange getPictureExchange() {
+        return new DirectExchange(PICTURE_EXCHANGE);
+    }
+
+    @Bean
+    public Binding jpgBinding() {
+        return BindingBuilder.bind(pictureImagesQueue())
+            .to(getPictureExchange())
+            .with(JPG_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding pngBinding() {
+        return BindingBuilder.bind(pictureImagesQueue())
+            .to(getPictureExchange())
+            .with(PNG_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding svgBinding() {
+        return BindingBuilder.bind(pictureVectorQueue())
+            .to(getPictureExchange())
+            .with(SVG_ROUTING_KEY);
     }
 
 }
