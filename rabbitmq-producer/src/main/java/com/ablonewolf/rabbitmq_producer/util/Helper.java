ee71db2f8fd36ab63b418c21606e323374412fe8
Arka Bhuiyan <belmont66@duck.com>
2025-01-10 18:37:29 +0600

    added methods and static list to generate random color, furniture, furniture name, and price

--- rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/util/Helper.java
+++ rabbitmq-producer/src/main/java/com/ablonewolf/rabbitmq_producer/util/Helper.java
@@ -2,9 +2,13 @@ package com.ablonewolf.rabbitmq_producer.util;
 
 import com.github.javafaker.Faker;
 
+import java.util.List;
+
 public class Helper {
 
     private static final Faker faker = Faker.instance();
+    private static final List<String> MATERIALS = List.of("wood", "plastic", "steel");
+    private static final List<String> COLORS = List.of("red", "green", "white");
 
     public static String generateRandomName() {
         return faker.name().firstName() + " " + faker.name().lastName();
@@ -20,6 +24,24 @@ public class Helper {
 
     public static Long generateRandomSize() {
         return faker.random().nextLong(10000);
+    }
+
+    public static String generateRandomColor() {
+        int index = faker.random().nextInt(COLORS.size());
+        return COLORS.get(index);
+    }
+
+    public static String generateRandomMaterial() {
+        int randomMaterial = faker.random().nextInt(MATERIALS.size());
+        return MATERIALS.get(randomMaterial);
+    }
+
+    public static String generateRandomFurnitureName() {
+        return faker.funnyName().name();
+    }
+
+    public static Integer generateRandomPrice() {
+        return faker.random().nextInt(5000, 10000);
     }
 }
 
